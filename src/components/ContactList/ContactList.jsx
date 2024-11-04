dcc5a8622d372c74dfcbf57cfd914476ef22484f
Alona Tiunina <163348743+tiunina@users.noreply.github.com>
2024-11-04 12:33:10 -0800

    Initial commit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,17 @@
+import Contact from "../Contact/Contact";
+
+const ContactList = ({ contactList, onDelete }) => {
+  return (
+    <ul>
+      {contactList.map((contactItem) => {
+        return (
+          <li key={contactItem.id}>
+            <Contact contactItem={contactItem} onDelete={onDelete} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default ContactList;
